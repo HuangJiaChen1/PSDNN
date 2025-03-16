3e19ca6ee286341eb7c1f99ffa32a9e541b97992
HuangJiaChen1 <124218571+HuangJiaChen1@users.noreply.github.com>
2025-03-15 21:52:59 -0600

    Update YCV.py

--- YCV.py
+++ YCV.py
@@ -239,7 +239,7 @@ class CACVIT(nn.Module):
     """ CntVit with VisionTransformer backbone
     """
 
-    def __init__(self, img_size=384, patch_size=16, in_chans=3,
+    def __init__(self, img_size=640, patch_size=16, in_chans=3,
                  embed_dim=1024, depth=24, num_heads=16,
                  decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
                  mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False, drop_path_rate=0):
